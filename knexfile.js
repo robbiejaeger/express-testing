aeefd5383fe1001443c54160c75c3c072ac9449f
Robbie Jaeger <jaeger.rob@gmail.com>
2018-05-03 08:51:54 -0600

    Remove testing

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,12 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/travis',
+    connection: 'postgres://localhost/penguins',
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
       directory: './db/seeds/dev'
-    },
-    useNullAsDefault: true
-  },
-
-  test: {
-    client: 'pg',
-    connection: 'postgres://localhost/travis_test',
-    migrations: {
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds/test'
-    },
-    useNullAsDefault: true
-  },
-
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + `?ssl=true`,
-    migrations: {
-      directory: './db/migrations'
     },
     useNullAsDefault: true
   }
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const database = require('knex')(configuration);
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (request, response) => {
-  response.send('Hello travis CI. Skip cleanup.');
+  response.send('Hello penguins.');
 });
 
 app.get('/api/v1/penguins', (request, response) => {
@@ -24,5 +24,3 @@ app.get('/api/v1/penguins', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`Travis test app running on port ${app.get('port')}`);
 });
-
-module.exports = app;
